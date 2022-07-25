dc006e26889e5a7f23f0604bcd3214d183ca4c53
Youssef Labtaimi <ylabtaim@e3r6p7.1337.ma>
2022-07-25 17:27:29 +0100

    ex05 edit

--- Module01/ex05/Harl.cpp
+++ Module01/ex05/Harl.cpp
@@ -6,24 +6,34 @@
 /*   By: ylabtaim <ylabtaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 12:28:47 by ylabtaim          #+#    #+#             */
-/*   Updated: 2022/07/25 15:05:45 by ylabtaim         ###   ########.fr       */
+/*   Updated: 2022/07/25 17:26:35 by ylabtaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-class Harl
-{
-private:
-	void debug( void );
-	void info( void );
-	void warning( void );
-	void error( void );
-public:
-	Harl();
-	~Harl();
-	void complain( std::string level );
-};
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::debug( void ) {
+	std::cout << "I love having extra bacon for my " <<
+	"7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info( void ) {
+	std::cout << "I cannot believe adding extra bacon costs more money." <<
+	" You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void Harl::warning( void ) {
+	std::cout << "I think I deserve to have some extra bacon for free." <<
+	" I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error( void ) {
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
 
 void Harl::complain( std::string level ) {
 	int	i = 0;
